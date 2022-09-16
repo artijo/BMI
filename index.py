36e4e24073eaf117ae9aa323563076927689d3be
ARTIJO <81065469+artijo@users.noreply.github.com>
2022-09-17 00:50:36 +0700

    Sent Data

--- index.py
+++ index.py
@@ -1,13 +1,28 @@
-from flask import Flask,render_template,request,session
-
+from flask import Flask,render_template,request,session,redirect
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mykey'
+
+
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
+@app.route('/test',methods=['GET','POST'])
+def testsm():
+    name = False
+    age = False
+    if request.method == 'POST':
+        name = request.form['name']
+        age = request.form['age']
+        session['name']=name
+    return render_template("test.html",name=name,age=age)
 
+@app.route('/logout')
+def logout():
+    session['name'] = None
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run()
