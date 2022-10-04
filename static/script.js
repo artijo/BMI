3f418f5c2b7486e97bad0ef74300bff521560f17
ARTIJO <81065469+artijo@users.noreply.github.com>
2022-10-04 17:54:32 +0700

    Change BG img & RM Gap CSS

--- static/script.js
+++ static/script.js
@@ -1,14 +1,3 @@
-/* Manage Class Funtions https://www.sitepoint.com/add-remove-css-class-vanilla-js/ */
-function addClass(e, l) {
-    elements = document.querySelectorAll(e);
-    for (var s = 0; s < elements.length; s++) elements[s].classList.add(l);
-}
-function removeClass(e, l) {
-    elements = document.querySelectorAll(e);
-    for (var s = 0; s < elements.length; s++) elements[s].classList.remove(l);
-}
-
-
 $(document).ready(function () {
     //change selectboxes to selectize mode to be searchable
        $("select").select2();
